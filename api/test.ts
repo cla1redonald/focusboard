a07c0920b02277d13b276b90567b3390348e8d21
Claire Donald <claired@moo.com>
2025-12-28 11:54:43 +0000

    Simplify test endpoint

--- api/test.ts
+++ api/test.ts
@@ -1,13 +1,3 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return res.status(200).json({
-    ok: true,
-    env: {
-      hasWebhookSecret: !!process.env.WEBHOOK_SECRET,
-      hasSupabaseUrl: !!process.env.SUPABASE_URL,
-      hasSupabaseKey: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
-      hasUserId: !!process.env.FOCUSBOARD_USER_ID,
-    }
-  });
+export default function handler(req: any, res: any) {
+  res.status(200).send("OK");
 }
